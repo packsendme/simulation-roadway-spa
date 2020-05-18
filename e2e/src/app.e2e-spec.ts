aa0329ee708d20f1e0a1a3c99063ec8502df5430
ricardomarzochi@gmail.com <r.libera.marzochi@accenture.com>
2020-05-18 22:38:58 +0200

    NewVersion/0.2.0

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('simulation-costs app is running!');
+    expect(page.getTitleText()).toEqual('simulation-roadway-spa app is running!');
   });
 
   afterEach(async () => {
